58584356141047cc0a168a27541896b188b3edeb
sandovalfenix <sandovalfenix@gmail.com>
2021-10-11 22:52:43 -0500

    Creacion de CRUD API completo SQLSERVER

--- Products.js
+++ Products.js
@@ -1,19 +1,92 @@
-/**
- * clase de producto basado en la base de datos SQL SERVER
- */
+const cnx = require('./cnx');
+const sql = require('mssql')
 
-class Products {
-    constructor(
-        id_product,
-		name_product,
-		price_product,
-		quantity_product
-    ) {
-        this.id_product,
-		this.name_product,
-		this.price_product,
-		this.quantity_product
+/**
+ * Lista de Productos
+ */
+async function getProducts(){
+    try {
+        let pool = await sql.connect(cnx)
+        let result = await pool.request().query('SELECT * FROM tbl_products')
+        return result.recordsets
+    }catch(err){
+        throw new Error (`Se presento el siguiente error: ${err.procName}... ${err.message}`)
     }
 }
 
-module.exports = Products;
+/**
+ * vista de un Producto
+ */
+ async function getProduct(idProduct){
+    try {
+        let pool = await sql.connect(cnx)
+        let result = await pool.request()
+        .input('id_product', sql.Int, idProduct)
+        .query('SELECT * FROM tbl_products WHERE id_product = @id_product')
+        return result.recordsets
+    }catch(err){
+        throw new Error (`Se presento el siguiente error: ${err.procName}... ${err.message}`)
+    }
+}
+
+/**
+ * Añadir Producto
+ */
+ async function newProduct(product){
+    try {
+        let pool = await sql.connect(cnx)
+        let newProduct = await pool.request()
+        .input('id_product', sql.Int, product.id_product)
+        .input('name_product', sql.VarChar, product.name_product)
+        .input('price_product', sql.Float, product.price_product)
+        .input('quantity_product', sql.Int, product.quantity_product)
+        .execute('pr_newProduct');
+
+        return newProduct.recordsets
+    }catch(err){
+        throw new Error (`Se presento el siguiente error: ${err.procName}... ${err.message}`)
+    }
+}
+
+/**
+ * Actualizar Producto
+ */
+ async function updateProduct(product){
+    try {
+        let pool = await sql.connect(cnx)
+        let updateProduct = await pool.request()
+        .input('id_product', sql.Int, product.id_product)
+        .input('name_product', sql.VarChar, product.name_product)
+        .input('price_product', sql.Float, product.price_product)
+        .input('quantity_product', sql.Int, product.quantity_product)
+        .execute('pr_updateProduct');
+
+        return updateProduct.recordsets
+    }catch(err){
+        throw new Error (`Se presento el siguiente error: ${err.procName}... ${err.message}`)
+    }
+}
+
+/**
+ * Eliminar Producto
+ */
+ async function deleteProduct(product){
+    try {
+        let pool = await sql.connect(cnx)
+        let deleteProduct = await pool.request()
+        .input('id_product', sql.Int, product.id_product)
+        .execute('pr_deleteProduct');
+
+        return deleteProduct.recordsets
+    }catch(err){
+        throw new Error (`Se presento el siguiente error: ${err.procName}... ${err.message}`)
+    }
+}
+
+module.exports = {
+    getProducts: getProducts,
+    getProduct: getProduct,
+    newProduct: newProduct,
+    updateProduct: updateProduct,
+    deleteProduct: deleteProduct
+}
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 console.log("inicio API");
-const DBProdcuts = require("./getProducts");
 const Products = require("./Products");
 
 var express = require("express");
-var bodyP = require("body-parser");
+var bodyParse = require("body-parser");
 var cors = require("cors");
 const { request, response } = require("express");
 
 var app = express();
 var router = express.Router();
 
-app.use(bodyP.urlencoded({ extended: true }));
-app.use(bodyP.json());
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParse.json());
 app.use(cors());
 
 app.use("/webApi", router);
@@ -20,7 +19,16 @@ app.use("/webApi", router);
  * ruta para obtener lista de productos
  */
 router.route("/products").get((request, response) => {
-  DBProdcuts.getProducts().then((result) => {
+  Products.getProducts().then((result) => {
+    response.json(result[0]);
+  });
+});
+
+/**
+ * ruta para leer un producto
+ */
+ router.route("/product/:idProduct").get((request, response) => {
+  Products.getProduct(request.params.idProduct).then((result) => {
     response.json(result[0]);
   });
 });
@@ -28,15 +36,41 @@ router.route("/products").get((request, response) => {
 /**
  * ruta para insertar un producto
  */
-router.route("/product/new").post((request, response) => {
-    let product = {...request.body}
-    DBProdcuts.newProduct(product).then((result) => {
-      response.json('Registro Completado');
-    }, (err) => {
-        console.log(err);
-        response.json(`Se presento el siguiente error: ${err.message}`);
-    });
+router.route("/product").post((request, response) => {
+  let product = {...request.body}
+  Products.newProduct(product).then((result) => {
+    response.json('Registro Completado');
+  }, (err) => {
+      console.log(err);
+      response.json(`Se presento el siguiente error: ${err.message}`);
   });
+});
+
+/**
+ * ruta para actualizar un producto
+ */
+ router.route("/product").put((request, response) => {
+  let product = {...request.body}
+  Products.updateProduct(product).then((result) => {
+    response.json('Actualizacion Completada');
+  }, (err) => {
+      console.log(err);
+      response.json(`Se presento el siguiente error: ${err.message}`);
+  });
+});
+
+/**
+ * ruta para delete un producto
+ */
+ router.route("/product").delete((request, response) => {
+  let product = {...request.body}
+  Products.deleteProduct(product).then((result) => {
+    response.json('Registro Eliminado');
+  }, (err) => {
+      console.log(err);
+      response.json(`Se presento el siguiente error: ${err.message}`);
+  });
+});
 
 var ports = process.env.PORT || 5000;
 app.listen(ports);
